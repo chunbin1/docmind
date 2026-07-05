27ab4f5d6bbaa92532e5f6cf945b8b4df6cc5f32
chunbin1 <545360100@qq.com>
2026-07-05 12:11:54 +0800

    test(client): 引入 vitest + happy-dom 测试基座

--- packages/client/src/lib/waterfall.test.ts
+++ packages/client/src/lib/waterfall.test.ts
@@ -1,4 +1,4 @@
-import { test } from 'node:test'
+import { test } from 'vitest'
 import assert from 'node:assert/strict'
 import { buildWaterfall } from './waterfall.ts'
 import type { SpanRecord } from '../types.ts'
